465adcc91437bf5ddc9620e86dd857f2a3db657c
Prince781 <princetonf@optonline.net>
2013-05-05 00:45:29 -0400

    Adding CSS code and header files.

---
+++ src/gplotter.h
@@ -0,0 +1,9 @@
+//gplotter.h
+#define GPLOTTER_VERSION 0.1
+const gchar *GPLOTTER_VERSION_STRING = "0.1 (Alpha)";
+const int numWindows = 1;
+
+const typedef struct {
+	int width;
+	int height;
+} GPLOTTER_WINDOW_GEOMETRY;

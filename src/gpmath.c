cb04eea9c561e065d3584510224239ec6d2635d5
Ben Iofel <iofelben@gmail.com>
2014-03-03 21:28:16 -0500

    fixed makefile, disabled compiled headers, & fixed C code

--- src/gpmath.c
+++ src/gpmath.c
@@ -1,13 +1,13 @@
 #include "gpmath.h"
-#include <stdlib.h>
 
-char *to_equation_text(polyn poly) {
-	char *eq = malloc(poly.length * 300);
+char *to_equation_text(polynomial poly) {
+	char *eq = malloc(7 + poly.length * (7+1+1+7+1));
 
 	strcpy(eq, "f(x) = ");
+
 	for (int i=0; i < poly.length; i++) {
-		char temp[10];
-		sprintf(temp, "%f%c^%f", poly.m[i].c, poly.m[i].v, poly.m[i].e);
+		char temp[7+1+1+7+1]; //float char ^ float [space]
+		sprintf(temp, "%3.3f%c^%3.3f ", poly.monos[i].coef, poly.monos[i].var, poly.monos[i].exp);
 		strcat(eq, temp);
 	}
 	

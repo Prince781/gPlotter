ce27c31f2e23cc34e4be08f7860f59392465e0a4
Prince781 <princetonf@optonline.net>
2015-03-30 19:25:01 -0400

    Removing u_types.h

--- src/util/u_types.h
+++
@@ -1,9 +0,0 @@
-#ifndef _U_TYPES_H
-#define _U_TYPES_H
-
-#define type_name(type) _Generic((type), \
-int: int, unsigned int: unsigned_int, long: long, \
-unsigned long: unsigned_long, double: double, long double: long_double, \
-void *: void_ptr)
-
-#endif

92d692a2590d844a64a022ebe413a12e5aeee38d
Prince781 <princetonf@optonline.net>
2015-04-02 11:58:37 -0400

    Adding program and console headers.

---
+++ src/core/program.h
@@ -0,0 +1,7 @@
+#ifndef _PROGRAM_H
+#define _PROGRAM_H
+
+#define PROGRAM_NAME		"gPlotter"
+#define GPLOTTER_VERSION	"0.1-alpha"
+
+#endif /* _PROGRAM_H */
---
+++ src/core/util/console.h
@@ -0,0 +1,15 @@
+#ifndef _CONSOLE_H
+#define _CONSOLE_H
+
+#define KNRM	"\x1B[0m"	// normal console color
+#define KRED	"\x1B[31m"
+#define KGRN	"\x1B[32m"
+#define KYEL	"\x1B[33m"
+#define KBLU	"\x1B[34m"
+#define KMAG	"\x1B[35m"
+#define KCYN	"\x1B[36m"
+#define KWHT	"\x1B[37m"
+#define KRST	"\033[0m"	// resets to previous console color
+
+
+#endif	/* _CONSOLE_H */

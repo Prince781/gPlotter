5c7adc8334cd644dd0ce7eed077e71da757d9470
Prince781 <princetonf@optonline.net>
2015-04-13 13:25:58 -0400

    Working on main window UI.

--- src/gui/gplotter_app_window.c
+++ src/gui/gplotter_app_window.c
@@ -9,9 +9,14 @@ static void gplotter_app_window_class_init(GPlotterAppWindowClass *class) { }
 GPlotterAppWindow *gplotter_app_window_new(GPlotterApp *app) {
 	GPlotterAppWindow *gp_win;
 	GtkWindow *win;
+
 	GtkWidget *headerbar;
 	GtkBuilder *hb_builder;
 	GObject *hb_obj;
+
+	GtkWidget *mw_container;
+	GtkBuilder *mwc_builder;
+	GObject *mwc_obj;	
 
 	gp_win = g_object_new(GPLOTTER_APP_WINDOW_TYPE, 
 		"application", app, 
@@ -25,6 +30,13 @@ GPlotterAppWindow *gplotter_app_window_new(GPlotterApp *app) {
 	headerbar = GTK_WIDGET(hb_obj);
 	gtk_window_set_titlebar(win, headerbar);
 	gtk_widget_show(headerbar);
+
+	// main window
+	mwc_builder = gtk_builder_new_from_resource("/org/gtk/gplotter/window.ui");
+	mwc_obj = gtk_builder_get_object(mwc_builder, "mw_container");
+	mw_container = GTK_WIDGET(mwc_obj);
+	gtk_container_add(GTK_CONTAINER(win), mw_container);
+	gtk_widget_show(mw_container);
 	
 	return gp_win;
 }

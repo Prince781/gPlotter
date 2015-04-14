8791cc8b7126b4a885150ddaa202e16713943596
Prince781 <princetonf@optonline.net>
2015-04-14 12:46:58 -0400

    Working on axes for graph.

--- src/gui/grid_area.c
+++ src/gui/grid_area.c
@@ -2,22 +2,34 @@
 #include "grid_area.h"
 #include "gplotter_app_window.h"
 
+#define gdk_rgba_grayscale(color) (GdkRGBA){\
+	.red = 0.21*color.red + 0.72*color.green + 0.07*color.blue,\
+	.green = 0.21*color.red + 0.72*color.green + 0.07*color.blue,\
+	.blue = 0.21*color.red + 0.72*color.green + 0.07*color.blue,\
+	.alpha = color.alpha\
+}
+
 gboolean grid_draw2d(GtkWidget *widget, cairo_t *cr, gpointer data) {
 	unsigned width, height;
 	struct gp_session *session;
 	char buf[128];
 	double center_x, center_y;
+	int start_x, start_y;
 	double diff_x, diff_y;
+	GdkRGBA grid_color;
 	
 	width = gtk_widget_get_allocated_width(widget);
 	height = gtk_widget_get_allocated_height(widget);
 	session = GPLOTTER_APP_WINDOW(data)->session;
 	diff_x = session->ui.xmax - session->ui.xmin;
 	diff_y = session->ui.ymax - session->ui.ymin;
+	grid_color = gdk_rgba_grayscale(session->ui.colors.axes);
+	grid_color.alpha *= 0.4;
 
 	center_x = -(session->ui.xmin / diff_x) * width;
 	center_y = -(session->ui.ymin / diff_y) * height;
-
+	start_x = (int)(center_x+0.5);
+	start_y = (int)(center_y+0.5);
 	cairo_save(cr);
 
 	// fill
@@ -29,27 +41,76 @@ gboolean grid_draw2d(GtkWidget *widget, cairo_t *cr, gpointer data) {
 	if (session->ui.show_axes) {
 		gdk_cairo_set_source_rgba(cr, &session->ui.colors.axes);
 		cairo_set_line_width(cr, 1);
-		cairo_move_to(cr, 0, height/2);
-		cairo_line_to(cr, width, height/2);
+		cairo_move_to(cr, 0, center_y);
+		cairo_line_to(cr, width, center_y);
+		cairo_stroke(cr);		
+		cairo_move_to(cr, center_x, 0);
+		cairo_line_to(cr, center_x, height);
 		cairo_stroke(cr);
-		cairo_move_to(cr, width/2, 0);
-		cairo_line_to(cr, width/2, height);
-		cairo_stroke(cr);
+
+		cairo_save(cr);
+		gdk_cairo_set_source_rgba(cr, &grid_color);
+		for (int x = start_x-40; x>0; x-=40) {
+			cairo_move_to(cr, x, 0);
+			cairo_line_to(cr, x, height);
+			cairo_stroke(cr);
+		}
+		for (int x = start_x+40; x<width; x+=40) {
+			cairo_move_to(cr, x, 0);
+			cairo_line_to(cr, x, height);
+			cairo_stroke(cr);
+		}
+		for (int y = start_y-40; y>0; y-=40) {
+			cairo_move_to(cr, 0, y);
+			cairo_line_to(cr, width, y);
+			cairo_stroke(cr);
+		}
+		for (int y = start_y+40; y<height; y+=40) {
+			cairo_move_to(cr, 0, y);
+			cairo_line_to(cr, width, y);
+			cairo_stroke(cr);
+		}
+		cairo_restore(cr);
 	}
 
 	// label axes
 	if (session->ui.label_axes) {
-		int start_x = (int)(center_x+0.5);
-		int start_y = (int)(center_y+0.5);
 		cairo_text_extents_t extents;
 
-		cairo_set_font_size(cr, 12);
+		cairo_set_font_size(cr, 10);
 		for (int x = start_x-40; x>0; x-=40) {
 			snprintf(buf, 128, "%.3g",
 				(double)(start_x-x)/start_x*session->ui.xmin);
 			cairo_text_extents(cr, buf, &extents);
 			cairo_move_to(cr, x - extents.width/2, 
-				height/2 + extents.height + 2);
+				center_y + extents.height + 2);
+			cairo_show_text(cr, buf);
+		}
+
+		for (int x = start_x; x<width; x+=40) {
+			snprintf(buf, 128, "%.3g",
+				(double)(x-start_x)/(width-start_x)*session->ui.xmax);
+			cairo_text_extents(cr, buf, &extents);
+			cairo_move_to(cr, x - extents.width/2, 
+				center_y + extents.height + 2);
+			cairo_show_text(cr, buf);
+		}
+
+		for (int y = start_y-40; y>0; y-=40) {
+			snprintf(buf, 128, "%.3g",
+				-(double)(start_y-y)/start_y*session->ui.ymin);
+			cairo_text_extents(cr, buf, &extents);
+			cairo_move_to(cr, center_x - extents.width - 2, 
+				y + extents.height/2);
+			cairo_show_text(cr, buf);
+		}
+
+		for (int y = start_y+40; y<height; y+=40) {
+			snprintf(buf, 128, "%.3g",
+				-(double)(y-start_y)/(height-start_y)*session->ui.ymax);
+			cairo_text_extents(cr, buf, &extents);
+			cairo_move_to(cr, center_x - extents.width - 2, 
+				y + extents.height/2);
 			cairo_show_text(cr, buf);
 		}
 	}

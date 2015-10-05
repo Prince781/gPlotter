db21470ad4de346d719c98cd08fde2e077d6bc50
Prince781 <princetonf@optonline.net>
2015-10-04 20:29:21 -0400

    Adding autotools; restructuring project.

--- Makefile
+++
@@ -1,7 +0,0 @@
-all:
-	$(MAKE) -C res/
-	/bin/cp res/*.c src/resources.c
-	$(MAKE) -C src/
-clean:
-	$(MAKE) -C res/ clean
-	$(MAKE) -C src/ clean
---
+++ autogen.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+# generates initial makefiles
+
+srcdir=`dirname $0`
+test -z "$srcdir" && srcdir=.
+
+(test -f $srcdir/src/main.c) || {
+	echo -n "**Error**: directory "\`$srcdir\'" does not look like the"
+	echo " top-level gplotter directory"
+	exit 1
+}
+
+touch ChangeLog
+touch INSTALL
+
+aclocal --install -I build/autotools || exit 1
+autoreconf --force --install -Wno-portability || exit 1
+
+# for use in gnome-builder
+if [ "$NOCONFIGURE" = "" ]; then
+        $srcdir/configure "$@" || exit 1
+
+        if [ "$1" = "--help" ]; then exit 0 else
+                echo "Now type \`make\' to compile" || exit 1
+        fi
+else
+        echo "Skipping configure process."
+fi
+
